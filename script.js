0bb13775e22a08476fb6ae8f93a7adbc41a8054b
unknown <elhadjoumartall10@gmail.com>
2023-08-05 16:32:42 +0200

    image slider

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-const caroursel = document.querySelector(".caroursel");
-
-let isDragging = false, startX, startScrollLeft;
-
-const dragStart = () => {
-    isDragging = true;
-    caroursel.classList.add("dragging");
-    // Records the ini
-    startX = e.pageX;
-    startScrollLeft = caroursel.scrollLeft;
-}
-
-const dragging = (e) => {
-    if(!isDragging) return // si isdragging est faux return depuis ça
-    caroursel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-
-const dragStop = () => {
-    isDragging = false;
-    caroursel.classList.remove("dragging");
-}
-caroursel.addEventListener("mousemove", dragging);
-caroursel.addEventListener("mousemove", dragging);
-document.addEventListener(mouseup, dragStop);
+var swiper = new Swiper(".slide-content", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
